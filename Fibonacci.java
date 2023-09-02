627a2fda271ffd2e6760786c3a56408c4d1cb109
Fidan Sadikhova <99359863+fid03@users.noreply.github.com>
2023-09-02 17:32:47 +0400

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+package PracticeTasks;
+
+import java.util.Arrays;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        System.out.println(fib(4));
+
+    }
+    static int fib(int n){
+        if(n==1 || n==2){
+            return 1;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+}
